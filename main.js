8582fc1b68f3a9c0974af974843b609ad12a5872
cute apple <34147956+cuteapple@users.noreply.github.com>
2019-10-29 15:10:34 +0800

    create top-level window

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true
 
 /**@type {BrowserWindow} */
-let mainWindow
+let mainWindow, notelistWindow
 
 /**@typedef {string} NoteId*/
 /**@type {Map<NoteId,BrowserWindow>} */
@@ -11,79 +11,90 @@ let notes = new Map()
 
 ipcMain.addListener('open-note', (sender, noteid) => open_note(noteid))
 function open_note(id) {
-	//check if already open
-	if (notes.has(id)) {
-		notes.get(id).focus()
-		console.log(`bring note ${id} to front`)
-		return
-	}
+    //check if already open
+    if(notes.has(id)) {
+        notes.get(id).focus()
+        console.log(`bring note ${id} to front`)
+        return
+    }
 
-	console.log(`opening note ${id}`)
-	let note = new BrowserWindow({
-		x: 30 + Math.round(Math.random() * 200),
-		y: 30 + Math.round(Math.random() * 200),
-		width: 100 + Math.round(Math.random() * 300),
-		height: 100 + 32 + Math.round(Math.random() * 300),
-		transparent: false, 
-		frame: false,
-		show: false,
-		backgroundColor: '#f0f',
-		parent: mainWindow,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	})
+    console.log(`opening note ${id}`)
+    let note = new BrowserWindow({
+        x: 30 + Math.round(Math.random() * 200),
+        y: 30 + Math.round(Math.random() * 200),
+        width: 100 + Math.round(Math.random() * 300),
+        height: 100 + 32 + Math.round(Math.random() * 300),
+        transparent: false,
+        frame: false,
+        show: false,
+        backgroundColor: '#f0f',
+        parent: mainWindow,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-	note.noteid = id
-	note.loadFile('note.html')
-	note.on('focus', () => {
-		//refersh z-order
-		notes.delete(note.noteid)
-		notes.set(note.noteid, note)
-		//Todo: observe change of notes
-		mainWindow.webContents.send('order-changed', [...notes.keys()])
-	})
-	note.on('closed', () => {
-		notes.delete(note.noteid)
-		//Todo: condition
-		//if (!notes.size) mainWindow.close() //no need when mainWindow is visible
-	})
-	notes.set(note.noteid, note)
+    note.noteid = id
+    note.loadFile('note.html')
+    note.on('focus', () => {
+        //refersh z-order
+        notes.delete(note.noteid)
+        notes.set(note.noteid, note)
+        //Todo: observe change of notes
+        notelistWindow.webContents.send('order-changed', [...notes.keys()])
+    })
+    note.on('closed', () => {
+        notes.delete(note.noteid)
+        //Todo: condition
+        //if (!notes.size) mainWindow.close() //no need when mainWindow is visible
+    })
+    notes.set(note.noteid, note)
 }
 
+
 function initialize() {
-	mainWindow = new BrowserWindow({
-		//x: -1, y: -1, width: 1, height: 1,
-		transparent: false,
+    mainWindow = new BrowserWindow({
+        x: -1000, y: -1000, width: 300, height: 400,
+        transparent: true,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    mainWindow.setIgnoreMouseEvents(true)
+    mainWindow.loadFile('app-icon.png')
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+
+    notelistWindow = new BrowserWindow({
+        transparent: false,
         frame: true,
+        parent: mainWindow,
         x: 800, y: 100, height: 300, width: 400,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	})
-	//mainWindow.setIgnoreMouseEvents(true)
-	mainWindow.loadFile('index.html')
-	mainWindow.on('closed', function () {
-		mainWindow = null
-	})
-	mainWindow.webContents.openDevTools()
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    notelistWindow.on('close', ev => { ev.preventDefault(); notelistWindow.hide() })
+    notelistWindow.loadFile('index.html')
 }
 app.on('ready', initialize)
 
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+app.on('window-all-closed', function() {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow()
-	}
+app.on('activate', function() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if(mainWindow === null) {
+        createWindow()
+    }
 })
